73fc94d28c32d717fd60a1744255ac7435623a07
David_Ric <dric6996@gmail.com>
2026-02-22 19:59:57 -0300

    finalizando 3

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/pga',
+  base: '/pga/',
   plugins: [react()],
   server: {
     port: 5173,
-    open: '/pga'
+    open: '/pga/'
   },
   preview: {
-    open: '/pga'
+    open: '/pga/'
   }
 });
